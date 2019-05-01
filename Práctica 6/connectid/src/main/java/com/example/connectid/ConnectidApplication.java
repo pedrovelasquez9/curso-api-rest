d0a3177c12e9637cf987df8954c91d8ca0e2fddf
Pedro Plasencia <pjpv9011@gmail.com>
2019-05-01 18:22:37 +0200

    prácticas de oauth y openid

---
+++ Práctica 6/connectid/src/main/java/com/example/connectid/ConnectidApplication.java
@@ -0,0 +1,13 @@
+package com.example.connectid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectidApplication.class, args);
+	}
+
+}
